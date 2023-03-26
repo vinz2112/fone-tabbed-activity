2df362141b545e7039856c9f9efcd9632eec670a
vinz2112 <hippiefishmalta@gmail.com>
2023-03-26 20:06:22 +0200

    2nd commit-push = "Transformations" means ...ktx2-4-1 (2-6-1 causes the 'x "Transformations"' problem)

--- app/src/main/java/com/example/fonetabbedactivity/ui/main/PageViewModel.kt
+++ app/src/main/java/com/example/fonetabbedactivity/ui/main/PageViewModel.kt
@@ -10,6 +10,7 @@ class PageViewModel : ViewModel() {
 
     private val _index = MutableLiveData<Int>()
     val text: LiveData<String> = Transformations.map(_index) {
+    //val text: LiveData<String> = myLiveData.map(_index) {
         "Hello world from section: $it"
     }
 
